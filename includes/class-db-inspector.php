18896fb09dca6c882807bd3994aeed33c5eb5368
SiphiweAI <aisiphiwe@gmail.com>
2025-11-06 13:51:29 +0200

    updates

--- includes/class-db-inspector.php
+++ includes/class-db-inspector.php
@@ -1,19 +1,15 @@
-
 <?php
 if (!defined('ABSPATH')) exit;
 
-
 class SCM_DB_Inspector {
 
+    public static function list_tables() {
+        global $wpdb;
+        return $wpdb->get_results("SHOW TABLES LIKE '{$wpdb->prefix}scm_%'", ARRAY_N);
+    }
 
-    public static function list_tables() {
-        global $wpdb;
-        return $wpdb->get_results("SHOW TABLES LIKE '{$wpdb->prefix}scm_%'", ARRAY_N);
-    }
-
-
-    public static function describe_table($table_name) {
-        global $wpdb;
-        return $wpdb->get_results("DESCRIBE {$wpdb->prefix}{$table_name}", ARRAY_A);
+    public static function describe_table($table_name) {
+        global $wpdb;
+        return $wpdb->get_results("DESCRIBE {$wpdb->prefix}{$table_name}", ARRAY_A);
     }
 }
